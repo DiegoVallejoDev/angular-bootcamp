b77aeae26cb8e7871b543ac340cfab577e01456a
DiegoVallejoDev <die.1702@hotmail.com>
2024-04-25 23:01:01 -0600

    homework-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Homework1Component } from './homework1/homework1.component';
+import { Homework2Component } from './homework2/homework2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Homework1Component
+    Homework1Component,
+    Homework2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/homework2/homework2.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homework2',
+  templateUrl: './homework2.component.html',
+  styleUrls: ['./homework2.component.css']
+})
+export class Homework2Component implements OnInit {
+
+  public letters: string[] = ['A', 'B', 'C'];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
