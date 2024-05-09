4059db2bfdd090e6f8dafc3d3c6eea9e57ec30a5
DiegoVallejoDev <die.1702@hotmail.com>
2024-05-09 10:47:32 -0600

    Module rename

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { Homework2Component } from './homework2/homework2.component';
 import { CapitalizeDirective } from './capitalize.directive';
 import { Homework3Component } from './homework3/homework3.component';
 import { Homework4Component } from './homework4/homework4.component';
-import { MyFirstModuleModule } from './myFirstModule/my-first.module';
+import { MyFirstModule } from './myFirstModule/my-first.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,7 @@ import { MyFirstModuleModule } from './myFirstModule/my-first.module';
     Homework3Component,
     Homework4Component,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, MyFirstModuleModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, MyFirstModule],
   providers: [],
   bootstrap: [AppComponent],
 })
