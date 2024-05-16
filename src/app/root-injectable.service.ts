531d0308f6a21e967718468b01882e3db623826c
DiegoVallejoDev <die.1702@hotmail.com>
2024-05-16 13:03:57 -0600

    remove unnecessary Injectable decorator

--- src/app/root-injectable.service.ts
+++ src/app/root-injectable.service.ts
@@ -1,6 +1,3 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
 export class RootInjectableService {
   private msg: string = 'I’m a variable inside a singleton';
   public getPersistentData(): string {
